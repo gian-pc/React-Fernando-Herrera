e6b711f78e59dfdf74d0331eea2e75992da4f126
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-07-16 00:36:20 -0500

    operador ternario

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,11 @@
+// operador ternario
+
+const activo = true;
+
+const mensaje = activo ? "Activo" : "Inactivo";
+const mensaje2 = !activo ? "Activo" : null;
+const mensaje3 = activo && "Activo";
+
+console.log(mensaje);
+console.log(mensaje2);
+console.log(mensaje3);
