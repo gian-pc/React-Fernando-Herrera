b778df0298ea121836730cb6a4408d15aa6ea4fb
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-07-16 15:30:53 -0500

    props - comunicación entre componentes

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -8,9 +8,14 @@ const getSaludo = (nombre) => {
   return "Hola " + nombre;
 };
 
-export const FirstApp = () => {
+export const FirstApp = ( {title, subTitle } ) => {
+
+  console.log(title);
+
   return (
     <>
+      <h1>{title}</h1>
+      <h2>{subTitle + 1}</h2>
       <h1>{getSaludo('Gian')}</h1>
       <code>{JSON.stringify(newMessage)}</code>
       <p>Soy un subtitulo</p>
